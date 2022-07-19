03fe946bbad56cc958aed838c51785fe6427ac26
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2022-07-19 14:12:36 +0300

    feat: add Task02 solution to homework26

--- src/homework26/Task02.java
+++ src/homework26/Task02.java
@@ -1,7 +1,34 @@
 package homework26;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task02 {
     public static void main(String[] args) {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
+        try {
+            System.out.println("Введите число: ");
+            getNumber(Integer.parseInt(input.readLine()));
+        } catch (NegativeNumberException | IllegalArgumentException | IOException e) {
+            System.out.println("Ошибка: " + e);
+        }
+    }
+
+    public static void getNumber(int n) throws NegativeNumberException {
+        if (n < 0) {
+            throw new NegativeNumberException();
+        } else if (n > 100) {
+            throw new IllegalArgumentException("Число больше ста.");
+        } else {
+            System.out.println(n);
+        }
+    }
+
+    public static class NegativeNumberException extends Exception {
+        public NegativeNumberException() {
+            super("Вы ввели отрицательное число.");
+        }
     }
 }
