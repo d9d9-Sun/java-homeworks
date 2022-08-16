13f50f72e3030d1de8792840ce80d2a87d1f2369
Dmitry Yarmakovich <d.yarmakovich@gmail.com>
2022-08-16 12:22:57 +0300

    feat: add main method and Menu

---
+++ src/homework30/General.java
@@ -0,0 +1,9 @@
+package homework30;
+
+import java.io.IOException;
+
+public class General {
+    public static void main(String[] args) throws IOException {
+        Menu.showMenu();
+    }
+}
---
+++ src/homework30/Menu.java
@@ -0,0 +1,53 @@
+package homework30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Menu {
+    public static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void showMenu() throws IOException {
+        System.out.println("Меню:\n" +
+                "1 - полная распечатка базы данных\n" +
+                "2 - распечатка данных по конкретному коду\n" +
+                "3 - распечатка данных по конкретному типу штрафа\n" +
+                "4 - распечатка данных по конкретному городу\n" +
+                "5 - добавление нового человека с информацией о нём\n" +
+                "6 - добавление новых штрафов для уже существующей записи\n" +
+                "7 - удаление штрафа\n" +
+                "8 - замена информации о человеке и его штрафах\n" +
+                "0 - закрыть программу" +
+                "Введите цифру меню: ");
+        int choice = Integer.parseInt(input.readLine());
+        switch (choice) {
+            case 1:
+                showDB();
+                break;
+            case 2:
+                showInfoByID();
+                break;
+            case 3:
+                showInfoByTicketType();
+                break;
+            case 4:
+                showInfoByCity();
+                break;
+            case 5:
+                addCitizen();
+                break;
+            case 6:
+                addTicket();
+                break;
+            case 7:
+                deleteTicket();
+                break;
+            case 8:
+                replaceInfo();
+                break;
+            default:
+                System.out.println("Неверный ввод.");
+                showMenu();
+        }
+    }
+}
